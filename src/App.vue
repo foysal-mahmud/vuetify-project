b32efc00781fddd537fa68f2df4fcb36dee5494d
Md Foysal Mahmud <foysal.mahmud@bjitgroup.com>
2025-06-10 12:11:55 +0600

    feat: initial project setup - Configure Vue 3 with Vuetify - Set up TypeScript configuration - Add development tools and configurations - Initialize project structure

---
+++ src/App.vue
@@ -0,0 +1,88 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+      v-model="drawer"
+      temporary
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in menuItems"
+          :key="i"
+          :prepend-icon="item.icon"
+          :title="item.title"
+          :to="item.to"
+        />
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
+      <v-app-bar-title>Vuetify Components Demo</v-app-bar-title>
+      <v-spacer />
+      <v-btn icon="mdi-theme-light-dark" @click="toggleTheme" />
+    </v-app-bar>
+
+    <v-main>
+      <v-container fluid>
+        <router-view />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+  import { ref } from 'vue'
+  import { useTheme } from 'vuetify'
+
+  const theme = useTheme()
+  const drawer = ref(false)
+
+  const toggleTheme = () => {
+    theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+  }
+
+  const menuItems = [
+    {
+      title: 'Layout Components',
+      icon: 'mdi-view-dashboard',
+      to: '/layout',
+    },
+    {
+      title: 'Navigation Components',
+      icon: 'mdi-menu',
+      to: '/navigation',
+    },
+    {
+      title: 'Form Components',
+      icon: 'mdi-form-select',
+      to: '/forms',
+    },
+    {
+      title: 'Data Display',
+      icon: 'mdi-table',
+      to: '/data-display',
+    },
+    {
+      title: 'UI Elements',
+      icon: 'mdi-card',
+      to: '/ui-elements',
+    },
+    {
+      title: 'Interactive Components',
+      icon: 'mdi-gesture-tap-button',
+      to: '/interactive',
+    },
+  ]
+</script>
+
+<style lang="scss">
+  .v-application {
+    .text-h5 {
+      margin-bottom: 1rem;
+    }
+
+    .section-gap {
+      margin-bottom: 2rem;
+    }
+  }
+</style>
